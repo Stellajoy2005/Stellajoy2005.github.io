a32911b7a663a234207ff2f8b7c64503c864465b
Stellajoy2005 <stellajoyrossi@gmail.com>
2025-02-10 09:01:09 -0500

    First version

---
+++ script.js
@@ -0,0 +1,20 @@
+Infinity;
+
+var kitty;
+
+kitty = 5;
+
+var Kitty = 24;
+
+console.log(kitty);
+
+kitty = Kitty;
+
+console.log(kitty);
+
+var doggy;
+console.log(doggy);
+
+var name = "Stella";
+
+console.log("Name:", name);
---
+++ sketch.js
@@ -0,0 +1,16 @@
+var x = 0;
+var y = 0;
+
+function setup(){
+    createCanvas(1425,150);
+}
+function draw(){
+    background('#dbe5ea');
+    text('© 2023 Stella Rossi. All Rights Reserved.',600,70)
+    text('Currently a Student Attending and Enrolled at OCAD University',550,90)
+    circle(x,y,10,10);
+    x = x + 1;
+    x = x % 1425;
+    y = y + 2
+    y = y % 150
+}
